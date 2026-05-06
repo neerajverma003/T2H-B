dd05eb2e21a99b43f7d5529afe39e1a734ceeb58
Mohito09 <mohitos0909@gmail.com>
2026-05-06 18:58:18 +0530

    Changes in env

--- src/config/allowedOrigin.js
+++ src/config/allowedOrigin.js
@@ -5,8 +5,13 @@ const allowedOrigins = [
   'http://localhost:3001',
   'http://192.168.68.106:5173',
   'http://192.168.68.114:3000',
-  'http://localhost:5173',
-  
-  
+  'http://api.trip2honeymoon.com',
+  'https://api.trip2honeymoon.com',
+  'http://trip2honeymoon.com',
+  'https://trip2honeymoon.com',
+  'http://www.trip2honeymoon.com',
+  'https://www.trip2honeymoon.com',
+  'http://admin.trip2honeymoon.com',
+  'https://admin.trip2honeymoon.com',
 ];
 export default allowedOrigins;
