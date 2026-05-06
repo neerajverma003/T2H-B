cf660cfc0156fedc59580d33de0f307641c25559
Mohito09 <mohitos0909@gmail.com>
2026-05-06 17:22:50 +0530

    Changes in Back2

--- src/config/allowedOrigin.js
+++ src/config/allowedOrigin.js
@@ -1,6 +1,6 @@
 const allowedOrigins = [
   'http://localhost:5173',
-  'http://localhost:5000',
+  'http://localhost:2000',
   'http://localhost:3000',
   'http://localhost:3001',
   'http://192.168.68.106:5173',
--- src/index.js
+++ src/index.js
@@ -75,7 +75,7 @@ app.use('/user', userRoute);
 /**
  * SERVER LIFECYCLE
  */
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 2000;
 app.listen(PORT, () => {
   console.log(`[SYSTEM] Server active on port ${PORT} in ${process.env.NODE_ENV || 'development'} mode`);
 });
